0bd5458e4b62616d84d1b289181ae3c705e18de6
vivianamarquez <vmpena@dons.usfca.edu>
2023-11-12 20:48:48 +0100

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Mamba
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/mamba-one-page-bootstrap-template-free/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
